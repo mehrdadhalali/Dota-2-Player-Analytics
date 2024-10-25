1d408df272330dfb2a7121e96ebe727b3d6ebb1c
mehrdadhalali <trainee.mehrdad.halali@sigmalabs.co.uk>
2024-10-25 10:21:39 +0100

    Made a working dashboard with one (1) graph.

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -15,14 +15,29 @@ def turn_into_dataframe(db_data: list[dict]) -> pd.DataFrame:
 
 def create_hero_bar_chart(df: pd.DataFrame) -> alt.Chart:
 
-    games_per_hero = df.groupby(["hero_name"]).count().reset_index()
-    games_per_hero = games_per_hero.sort_values(
-        by=["game_mode_id"], ascending=False)
+    games_per_hero = df["hero_name"].value_counts().reset_index()
     games_per_hero = games_per_hero[:10]
+    top_10_heroes = games_per_hero["hero_name"].tolist()
+    hero_wl = df.groupby(["hero_name", "won"]).size().reset_index()
+    hero_wl = hero_wl[hero_wl["hero_name"].isin(top_10_heroes)]
+    hero_wl.columns = ["hero_name", "won", "count"]
 
-    hero_bar_chart = alt.Chart(games_per_hero).mark_bar().encode(
+    hero_bar_chart = alt.Chart(hero_wl,
+                               title=alt.Title("Most Played Heroes", color="#FFFFFF",
+                                               anchor="middle")).mark_bar().encode(
         x=alt.X("hero_name", title="Hero").sort("-y"),
-        y=alt.Y("game_mode_id", title="Number of games")
+        y=alt.Y("count", title="Number of games"),
+        color=alt.Color("won", scale=alt.Scale(range=['#ff0000', '#32CD32']))
     )
 
     return hero_bar_chart
+
+
+def calculate_win_rate(df: pd.DataFrame) -> float:
+    """Calculates player's win rate."""
+
+    win_count = len(df[df["won"]])
+
+    win_rate = win_count/(len(df))
+
+    return round(win_rate*100, 2)
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,14 +1,37 @@
 """The dashboard"""
 
-import streamlit as st
+from os import environ as ENV
 
-from charts import create_hero_bar_chart, turn_into_dataframe
+import streamlit as st
+from dotenv import load_dotenv
+
+from charts import create_hero_bar_chart, turn_into_dataframe, calculate_win_rate
 from database import get_all_data
+
+load_dotenv()
+
+st.set_page_config(layout="wide")
 
 if __name__ == "__main__":
 
     db_data = get_all_data()
 
     data = turn_into_dataframe(db_data)
+
+    cols = st.columns([2, 0.5])
+    with cols[0]:
+        st.header(
+            f"Welcome to your Dota 2 Analytics Dashboard, {ENV['PLAYER_NAME']}.")
+        sub_cols = st.columns([1, 1, 1])
+        with sub_cols[0]:
+            wr_text = f"Win Rate: {calculate_win_rate(data)}%"
+            st.markdown(f"<h3 style='text-align: center'> {wr_text} </h3>",
+                        unsafe_allow_html=True)
+    with cols[1]:
+        st.image(ENV["PLAYER_AVATAR"])
+
+    st.markdown(
+        "<style>.line { border: 0.5px solid; margin: 0; }</style><div class='line'></div>",
+        unsafe_allow_html=True)
 
     st.altair_chart(create_hero_bar_chart(data))
